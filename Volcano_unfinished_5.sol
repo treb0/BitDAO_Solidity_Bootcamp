51d65c32d5d6af992ee1a50694ba521be8f63d62
Matias <86385686+treb0@users.noreply.github.com>
2021-11-19 12:31:58 -0300

    Update Volcano_unfinished_5.sol

--- Volcano_unfinished_5.sol
+++ Volcano_unfinished_5.sol
@@ -7,30 +7,53 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.4
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.4/contracts/access/Ownable.sol";
 
-contract VolcanoCoin is ERC20, Ownable {
+contract FIFACoin is ERC20, Ownable {
     
-    uint public total_supply = 10000;
+    uint public initial_supply = 10000;
     
-    address owner;
+    //address owner;
     
     string private _name;
     string private _symbol;
 
-    constructor() ERC20('Volcano','VOL') {
-        owner = msg.sender;
+    constructor() ERC20('FIFACoin','FIFA') {
+        //owner = msg.sender;
         //balance[owner] = total_supply;
+        
+        //8. ERC20 has an internal function _mint . When called, it mints token to the recipient. Create a constructor that calls the _mint function inside the constructor. Mint the 10000 token supply to the owner.
+        _mint(Ownable.owner(),initial_supply);
+
     }
     
-    event newSupply (uint);
+    // 9. Make a function that can mint more tokens to the owner.
     
-    function increase_supply(uint addSupply) public onlyOwner {
-        
-        total_supply = total_supply + addSupply;
-        
-        emit newSupply(total_supply);
+    function mintToOwner(uint _amount) public {
+        _mint(Ownable.owner(),_amount);
     }
     
+    struct Payment{
+        address recipient;
+        uint transferAmount;
+    }
     
+    // 10. Firstly, make your payment record mapping public so that view calls can be made to read the contract’s data.
     
+    mapping (address => Payment[]) public payments;
+    
+    // 11. For the payments record mapping, create a function that takes the sender’s address, the receiver’s address and amount as an input, then creates a new payment record when a transfer is made and adds the new record to the user’s payment record.
+    
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        payments[_msgSender()].push(Payment(recipient, amount));
+        return true;
+    }
+    
+    // get your Payments in better format
+    
+    function getAllPayments() public view returns (Payment[] memory) {
+        
+        return payments[msg.sender];
+        
+    }
     
 }
